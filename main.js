8475bf0490f8c902a08fe67a5adcbebc2d06b102
Tkzito <tkzito84@gmail.com>
2023-06-13 20:10:59 -0300

    Janela do Chat GPT suspença para facilitar o uso nas pesquisas

---
+++ main.js
@@ -0,0 +1,115 @@
+const path = require("path");
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+const screen = require("electron").screen;
+
+let janela;
+let isMinimized = false;
+
+const tamanhojanela = {
+    width: 500,
+    height: 500,
+};
+
+const abrirOuFecharJanela = (status) => {
+    if (status === "show") {
+        janela.restore();
+        isMinimized = false;
+    } else {
+        janela.minimize();
+        isMinimized = true;
+    }
+};
+
+const criarJanela = () => {
+    janela = new BrowserWindow({
+        width: tamanhojanela.width,
+        height: screen.getPrimaryDisplay().workAreaSize.height,
+        x: screen.getPrimaryDisplay().workAreaSize.width - tamanhojanela.width,
+        y: 30,
+        alwaysOnTop: true,
+        transparent: true,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            preload: path.resolve("./preload.js"),
+            nativeWindowOpen: true,
+        },
+    });
+
+    const menuTemplate = [
+        {
+            label: "File",
+            submenu: [
+                {
+                    label: "Minimize",
+                    accelerator: "CmdOrCtrl+M",
+                    click: () => {
+                        abrirOuFecharJanela(isMinimized ? "show" : "hide");
+                    },
+                },
+                { type: "separator" },
+                { role: "quit" },
+            ],
+        },
+    ];
+
+    const menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+};
+
+const aplicarNovosEstilos = () => {
+    janela.webContents.insertCSS(`
+        main {
+            padding: 5px;
+        }
+        .bg-gray-50 {
+            --tw-gb-opacity: 1;
+            background-color: #000;
+            border-radius: 30px;
+            margin: 5px;
+        }
+        .bg-gray-50 + div {
+            --tw-bg-opacity: 1;
+            background-color: #000;
+            border-radius: 30px;
+            margin: 5px;
+        }
+        img {
+            height: 109px;
+            width: 147px;
+            z-index: 999;
+            position: fixed;
+            border-radius: 50%;
+            right: 0px;
+            filter: initial;
+            clip-path: CSSNumericValue(21%);
+            cursor: pointer;
+        }
+    `);
+};
+
+const carregarOpenIA = () => {
+    janela.loadURL("https://chat.openai.com/chat");
+};
+
+app.whenReady().then(() => {
+    criarJanela();
+    carregarOpenIA();
+
+    janela.webContents.on("did-finish-load", () => {
+        aplicarNovosEstilos();
+    });
+
+    // Capturar evento de atalho de teclado
+    janela.on("before-input-event", (event, input) => {
+        if (input.key === "m" && (input.control || input.meta)) {
+            abrirOuFecharJanela(isMinimized ? "show" : "hide");
+            event.preventDefault(); // Impedir o comportamento padrão do atalho
+        }
+    });
+});
+
+ipcMain.on("AbrirOuFecharJanela", (event, status) => {
+    abrirOuFecharJanela(status);
+});
---
+++ start-app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Sda/servidor-cliente/gpt
+npm start
